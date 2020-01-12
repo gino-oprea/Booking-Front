8574e49653b7ee1512659184b936fe42822dd709
Gino <gino.oprea24@gmail.com>
2020-01-12 13:07:27 +0200

    fix

--- src/app/objects/booking-entity.ts
+++ src/app/objects/booking-entity.ts
@@ -3,11 +3,12 @@ export class BookingEntity
 {
   constructor(
     public idEntity?: number,
-    public isAutoAssigned?: boolean,    
-    public idLevel?: number,        
+    public isAutoAssigned?: boolean,
+    public idLevel?: number,
+    public idLevelType?: number,
     public isMultipleBooking?: boolean,
     public images?: Image[],
-    
+
     public entityName_RO?: string,
     public entityName_EN?: string,
     public levelName_RO?: string,
