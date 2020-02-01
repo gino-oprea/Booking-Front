f1fae2fe79c1211fd928520457c6765e1e921e56
Gino <gino.oprea24@gmail.com>
2020-02-01 13:56:58 +0200

    recaptcha fix

--- src/app/shared/generic-confirm-dialog/generic-confirm-dialog.component.ts
+++ src/app/shared/generic-confirm-dialog/generic-confirm-dialog.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit, Injector, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Injector, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild, OnDestroy } from '@angular/core';
 import { BaseComponent } from '../base-component';
 import { RecaptchaService } from '../../app-services/recaptcha.service';
 import { Actions } from 'app/enums/enums';
+import { RecaptchaComponent } from 'ng-recaptcha';
 
 @Component({
   selector: 'bf-generic-confirm-dialog',
   templateUrl: './generic-confirm-dialog.component.html',
   styleUrls: ['./generic-confirm-dialog.component.css']
 })
-export class GenericConfirmDialogComponent extends BaseComponent implements OnInit, OnChanges
+export class GenericConfirmDialogComponent extends BaseComponent implements OnInit, OnChanges, OnDestroy
 {
-  
+
   @Input() confirmMessage: string = "Are you sure?";
   @Input() useCaptcha: boolean = false;
   @Input() resetCaptcha: boolean = false;
   @Output() onConfirm = new EventEmitter<string>();
 
+  captchaDestroyed: boolean = false;
+
   validCaptcha: boolean = false;
 
   constructor(private injector: Injector,
-  private recaptchaService:RecaptchaService)
+    private recaptchaService: RecaptchaService)
   {
     super(injector,
       ['lblYes',
         'lblNo']);
   }
 
+  ngOnDestroy()
+  {
+    this.captchaDestroyed = true;
+  }
+
   ngOnInit() 
   {
+    this.captchaDestroyed = false;
   }
   ngOnChanges(changes: SimpleChanges): void
   {
