17e3b6d23f44e43f08933362e591169d9f0ae5dc
gino-oprea <31820180+gino-oprea@users.noreply.github.com>
2020-02-26 22:12:24 +0200

    dynamic roles

---
+++ src/app/objects/site-module.ts
@@ -0,0 +1,10 @@
+export class SiteModule
+{
+  constructor(
+    public id?: number,
+    public idSite?: number,
+    public siteName?: string,
+    public name?: string,
+    public isOption?: boolean
+  ) { }
+}
--- src/app/route-guards/role.guard.ts
+++ src/app/route-guards/role.guard.ts
@@ -3,30 +3,62 @@ import { Injectable } from '@angular/core';
 import { LoginService } from '../app-services/login.service';
 import { Observable } from 'rxjs';
 import { UserRoleEnum } from '../enums/enums';
+import { COMPANY_ROUTES } from 'app/back/company.routing';
 
 @Injectable()
 export class RoleGuard implements CanActivate
 {
+  backOfficeRoutesWithModule: any[];
+
   constructor(private loginService: LoginService,
-    private router: Router) { }
+    private router: Router)
+  {     
+    
+  }
+
+  getFirstRouteWithModuleRights(idCompany:number):string
+  {
+    let resultRoute: string = '';
+    for (let i = 0; i < COMPANY_ROUTES[0].children.length; i++) 
+    {
+      if (COMPANY_ROUTES[0].children[i].path != '')
+      {
+        let route = COMPANY_ROUTES[0].children[i];
+        let requiredModule = route.data.module;
+        let currentUser = this.loginService.getCurrentUser();
+        let role = currentUser.rolesWithModules.find(r => r.idCompany == idCompany);
+        if (role != null && role.siteModules.find(m => m.name == requiredModule) != null)
+        {
+          resultRoute = route.path;
+          break;
+        }
+      }
+    }
+       
+    return resultRoute;
+  }
 
   public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean 
   {
-    //return true;
+    //return true;//de scos dupa modificari    
 
-    let requiredRole = route.data.role as string;
+    let requiredModule = route.data.module as string;
     let idCompany = route.parent.params["id"];
     let currentUser = this.loginService.getCurrentUser();
-    let role = currentUser.roles.find(r => r.idCompany == idCompany);
+    let role = currentUser.rolesWithModules.find(r => r.idCompany == idCompany);
     if (role)
     {
-      if (role.idRole <= UserRoleEnum[requiredRole])
+      if (role.siteModules.find(m => m.name == requiredModule))
         return true;
       else
-        if (role.idRole == UserRoleEnum.Employee)
-          this.router.navigate(["/company/" + idCompany + "/bookings"]);
+      {
+        //de gandit o solutie de routare la primul modul pe care are permisiuni
+        let path = this.getFirstRouteWithModuleRights(idCompany);
+        if (path != '')
+          this.router.navigate(["/company/" + idCompany + "/" +path]);          
         else
           this.router.navigate(['/searchcompany']);
+      }
     }
     else
       this.router.navigate(['/searchcompany']);
