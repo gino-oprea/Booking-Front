1d4d1f79c5d93b1ec694ba5aec405b0151fd0d52
gino-oprea <31820180+gino-oprea@users.noreply.github.com>
2020-02-29 20:10:42 +0200

    build fix

--- src/app/route-guards/role.guard.ts
+++ src/app/route-guards/role.guard.ts
@@ -2,28 +2,45 @@ import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, Activ
 import { Injectable } from '@angular/core';
 import { LoginService } from '../app-services/login.service';
 import { Observable } from 'rxjs';
-import { UserRoleEnum } from '../enums/enums';
-import { COMPANY_ROUTES } from 'app/back/company.routing';
+// import { COMPANY_ROUTES } from 'app/back/company.routing';
 
 @Injectable()
 export class RoleGuard implements CanActivate
 {
+  COMPANY_ROUTES: any[];
   backOfficeRoutesWithModule: any[];
 
   constructor(private loginService: LoginService,
     private router: Router)
   {     
-    
+    this.COMPANY_ROUTES = [
+      {        
+        children: [         
+          { path: 'generaldetails', data: { module: 'Company general details' } },
+          { path: 'levels', data: { module: 'Company levels' } },
+          { path: 'entities', data: { module: 'Company entities' } },
+          { path: 'timetables', data: { module: 'Company timetables' } },
+          { path: 'entities/:idLevel/:idEntity', data: { module: 'Company entities' } },
+          { path: 'bookings', data: { module: 'Company bookings' } },
+          { path: 'bookings-history', data: { module: 'Company bookings history' } },
+          { path: 'clients', data: { module: 'Company clients' } },
+          { path: 'company-security', data: { module: 'Company security' } },
+          { path: 'level-linking', data: { module: 'Company level linking' } },
+          { path: 'dashboard', data: { module: 'Company dashboard' } },
+          { path: 'company-logs', data: { module: 'Company logs' } }          
+        ]
+      }
+    ];
   }
 
   getFirstRouteWithModuleRights(idCompany:number):string
   {
     let resultRoute: string = '';
-    for (let i = 0; i < COMPANY_ROUTES[0].children.length; i++) 
+    for (let i = 0; i < this.COMPANY_ROUTES[0].children.length; i++) 
     {
-      if (COMPANY_ROUTES[0].children[i].path != '')
+      if (this.COMPANY_ROUTES[0].children[i].path != '')
       {
-        let route = COMPANY_ROUTES[0].children[i];
+        let route = this.COMPANY_ROUTES[0].children[i];
         let requiredModule = route.data.module;
         let currentUser = this.loginService.getCurrentUser();
         let role = currentUser.rolesWithModules.find(r => r.idCompany == idCompany);
